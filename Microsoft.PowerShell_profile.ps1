ef31f5fb31820a88d4e56061246f6f1bc46d5ca0
Michael Nye <mnye11@icloud.com>
2023-08-11 11:55:11 -0400

    Added parsing git branch into prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,18 +5,28 @@ Version:        1.1
 Description:    Script contains PowerShell profile that can be used on Windows or macOS
 Change Log:	    v1.0: Initial Release
                 v1.1: Added prompt function
+                v1.2: Added AWS CLI auto complete
+                v1.3: Added parsing git branch to prompt for Windows
 Notes:          Windows CurrentUserCurrentHost profile location ($PROFILE): C:\Users\<user>\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
                 MacOS profile location: /Users/<user>/.config/powershell/Microsoft.PowerShell_profile.ps1
 #>
 
 # Customize PS Prompt
-# Similar to Ubuntu prompt - user@computer:<pwd>$
+# Similar to Ubuntu prompt - user@computer:<pwd> (git branch)$
 # 'user@computer in bold green, pwd in bold blue. Computername in lower case. Replaces $home with '~'.
 Function Prompt {
     if ($IsWindows) {
-        "$($PSStyle.Foreground.Green)$($PSStyle.Bold)$env:USERNAME@$($env:COMPUTERNAME.ToLower())$($PSStyle.Reset):$($PSStyle.Foreground.Blue)$($PSStyle.Bold)$($(Get-Location).Path.replace($home,'~'))$($PSStyle.Reset)$ "
-        # This line can be used instead if you need the ANSI escape sequences.  $PSStyle was added in PowerShell 7.2
-        #"`e[32m`e[1m$env:USERNAME@$($env:COMPUTERNAME.ToLower())`e[0m:`e[34m`e[1m$($(Get-Location).Path.replace($home,'~'))`e[0m$ "
+        $gitbranch = Get-GitBranch
+        if ($gitbranch) {
+            "$($PSStyle.Foreground.Green)$($PSStyle.Bold)$env:USERNAME@$($env:COMPUTERNAME.ToLower())$($PSStyle.Reset):$($PSStyle.Foreground.Blue)$($PSStyle.Bold)$($(Get-Location).Path.replace($home,'~')) ($($gitbranch))$($PSStyle.Reset)$ "
+            # This line can be used instead if you need the ANSI escape sequences.  $PSStyle was added in PowerShell 7.2
+            #"`e[32m`e[1m$env:USERNAME@$($env:COMPUTERNAME.ToLower())`e[0m:`e[34m`e[1m$($(Get-Location).Path.replace($home,'~'))`e[0m$ "
+        }
+        else {
+            "$($PSStyle.Foreground.Green)$($PSStyle.Bold)$env:USERNAME@$($env:COMPUTERNAME.ToLower())$($PSStyle.Reset):$($PSStyle.Foreground.Blue)$($PSStyle.Bold)$($(Get-Location).Path.replace($home,'~'))$($PSStyle.Reset)$ "
+            # This line can be used instead if you need the ANSI escape sequences.  $PSStyle was added in PowerShell 7.2
+            #"`e[32m`e[1m$env:USERNAME@$($env:COMPUTERNAME.ToLower())`e[0m:`e[34m`e[1m$($(Get-Location).Path.replace($home,'~'))`e[0m$ "
+        }
     }
     elseif ($IsMacOS) {
         # These lines can be used on macOS where environment variables are different than Windows
@@ -25,6 +35,7 @@ Function Prompt {
     }    
 }
 
+# Auto complete for AWS CLI
 Register-ArgumentCompleter -Native -CommandName aws -ScriptBlock {
     param($commandName, $wordToComplete, $cursorPosition)
         $env:COMP_LINE=$wordToComplete
@@ -37,4 +48,9 @@ Register-ArgumentCompleter -Native -CommandName aws -ScriptBlock {
         }
         Remove-Item Env:\COMP_LINE     
         Remove-Item Env:\COMP_POINT  
+}
+
+# Parse git branch to add to prompt
+function Get-GitBranch {
+    return git branch --show-current
 }
